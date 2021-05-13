99882b33f9e7b260a4c7cd469273f2cad749e6a3
Awshaf Ishtiaque <aishtiaq@sfu.ca>
2021-05-13 17:52:25 +0600

    styling added with css

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React, { useState } from "react";
 import data from "./data";
 import SingleQuestion from "./Question";
 function App() {
-  return <main className='container mt-5 -flex justify-content-center bg-light text-dark'>
-    <h2>
-      Questions And Answers About Login
-    </h2>
-    <div className='questionsContainer'>
-      <h4>q1</h4>
-      <h4>q2</h4>
-      <h4>q3</h4>
-    </div>
-    </main>;
+  return (
+    <main id='largerWrapper' className="shadow-lg p-5 container mt-5 border border-primary bg-light text-dark">
+
+      <div id='questionsBox' className='border border-secondary'>
+        <h2>Questions And Answers About Login</h2>
+      </div>
+
+      <div id='questionsContainer' className="border border-secondary">
+        <h4>q1</h4>
+        <h4>q2</h4>
+        <h4>q3</h4>
+      </div>
+
+    </main>
+  );
 }
 
 export default App;
